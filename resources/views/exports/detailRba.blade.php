c13e18441f44d498f8345d827a631f4f82207627
muhammadikhsan94 <muhammadikhsan208@gmail.com>
2022-12-27 11:21:44 +0700

    update

--- resources/views/exports/detailRba.blade.php
+++ resources/views/exports/detailRba.blade.php
@@ -1,11 +1,13 @@
 <table>
     <thead>
-        <th>KODE</th>
-        <th>NAME</th>
-        <th>VOLUME</th>
-        <th>SATUAN</th>
-        <th>INDIKATOR</th>
-        <th>TARIF</th>
+        <tr>
+            <th>SAMPLE KODE</th>
+            <th>SAMPLE NAME</th>
+            <th>VOLUME</th>
+            <th>SATUAN</th>
+            <th>INDIKATOR</th>
+            <th>TARIF</th>
+        </tr>
     </thead>
     <tbody>
         @foreach($akun AS $item)
